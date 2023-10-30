7f0b654eb9eb3b78bded677ea7b98bb88df9ae57
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-10-31 01:24:14 +0200

    k

---
+++ src/components/TaskCounter/TaskCounter.jsx
@@ -0,0 +1,10 @@
+import { CounterContainer } from "./TaskCounter.styled"
+
+export const TaskCounter = () => {
+    return (
+        <CounterContainer>
+            <p>Active: </p>
+            <p>Completed: </p>
+        </CounterContainer>
+    )
+}
---
+++ src/components/TaskCounter/TaskCounter.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const CounterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 10px;
+
+  p {
+    margin: 5px 0;
+    font-size: 14px;
+  }
+`;
