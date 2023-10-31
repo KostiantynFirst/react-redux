f8b4e952806d709218e56092f8c39414ac0d93c4
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-01 01:22:53 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,3 @@
-export const getStatusFilter = state => state.filters.status
-export const getTasks = state => state.tasks
+export const getTasks = state => state.tasks;
+export const getStatusFilter = state => state.filters.status;
+
