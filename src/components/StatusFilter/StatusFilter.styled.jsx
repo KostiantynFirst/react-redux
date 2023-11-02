7c980a4d6206f8b28ba29f7243b7304be605e00d
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-02 22:10:43 +0200

    Update StatusFilter.styled.jsx

--- src/components/StatusFilter/StatusFilter.styled.jsx
+++ src/components/StatusFilter/StatusFilter.styled.jsx
@@ -3,12 +3,7 @@ import styled from 'styled-components';
 export const FilterContainer = styled.div`
   display: flex;
   gap: 8px;
-  justify-content: center; /* Центрирование по горизонтали */
-  align-items: center; /* Центрирование по вертикали */
+  justify-content: center; 
+  align-items: center; 
   margin-bottom: 16px;
-
-  /* @media (max-width: 768px) {
-    flex-direction: column; 
-    align-items: flex-start; 
-  } */
 `;
