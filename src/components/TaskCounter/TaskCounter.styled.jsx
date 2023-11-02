e7693a7e316999eee1972de0ffa8be79e5b938ad
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-11-02 21:14:50 +0200

    l

--- src/components/TaskCounter/TaskCounter.styled.jsx
+++ src/components/TaskCounter/TaskCounter.styled.jsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
 export const CounterContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: left;
-  margin-bottom: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* Центрирование по горизонтали */
+    margin-bottom: 10px;
 `;
 
 export const CounterContainerParagraph = styled.p`
     margin: 5px 0;
     font-size: 14px;
+    text-align: center; /* Центрирование текста */
 `;
